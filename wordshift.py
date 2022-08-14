5aa7db0a6a41bd1952f468ebb811d2f819d1080a
rthom9 <thomrobert9@gmail.com>
2022-08-13 20:31:08 -0600

    Shift function if-else statements

--- wordshift.py
+++ wordshift.py
@@ -42,25 +42,25 @@ class game_board:
 
     def right_shift(self, row):
         if row == 1:
-            if self.board[5] == "   ":
+            if self.board[5] != "   ":
                 return "Shift not allowed"
             else:
                 self.board.insert(0, "   ")
                 self.board.pop(6)
         if row == 2:
-            if self.board[11] == "   ":
+            if self.board[11] != "   ":
                 return "Shift not allowed"
             else:
                 self.board.insert(6, "   ")
                 self.board.pop(12)
         if row == 3:
-            if self.board[17] == "   ":
+            if self.board[17] != "   ":
                 return "Shift not allowed"
             else:
                 self.board.insert(12, "   ")
                 self.board.pop(18)
         if row == 4:
-            if self.board[23] == "   ":
+            if self.board[23] != "   ":
                 return "Shift not allowed"
             else:
                 self.board.insert(18, "   ")
@@ -69,23 +69,34 @@ class game_board:
 
     def left_shift(self, row):
         if row == 1:
-            self.board.pop(0)
-            self.board.insert(4, "   ")
+            if self.board[0] != "   ":
+                return "Shift not allowed"
+            else:
+                self.board.pop(0)
+                self.board.insert(5, "   ")
         if row == 2:
-            self.board.pop(6)
-            self.board.insert(10, "   ")
+            if self.board[6] != "   ":
+                return "Shift not allowed"
+            else:
+                self.board.pop(6)
+                self.board.insert(10, "   ")
         if row == 3:
-            self.board.pop(12)
-            self.board.insert(16, "   ")
+            if self.board[12] != "   ":
+                return "Shift not allowed"
+            else:
+                self.board.pop(12)
+                self.board.insert(16, "   ")
         if row == 4:
-            self.board.insert(23, "  ")
-            self.board.pop(18)
+            if self.board[23] != "   ":
+                return "Shift not allowed"
+            else:
+                self.board.insert(23, "  ")
+                self.board.pop(18)
         # self.print_board()
 
 board_1 = game_board()
 board_1.shuffle_board()
-board_1.left_shift(2)
-board_1.left_shift(4)
 board_1.right_shift(1)
-print(board_1.right_shift(1))
+board_1.left_shift(1)
+board_1.left_shift(1)
 board_1.print_board()
